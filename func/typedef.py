89beffabd16a8c48a8800bb255e33a59092d8552
Oliver-noah-Jacob <zeamaysmoney@yahoo.co.jp>
2021-01-31 14:39:31 +0900

    add Namedtuple representing return

--- func/typedef.py
+++ func/typedef.py
@@ -1,12 +1,21 @@
 import os
 import pathlib
 import time
-from typing import List, Union
+from typing import List, NamedTuple ,Union
 
 from cachecontrol import CacheControl 
 from cachecontrol.caches import FileCache
+import requests
 
 PathSegments = Union[
     str, pathlib.Path, os.PathLike, 
     List[str], List[pathlib.Path], List[os.PathLike]
     ]
+
+class OpenAccessReturn(NamedTuple):
+    url: str
+    status_code: int
+    responce: dict
+
+    def __str__(self) ->str:
+        return f"url: {self.url}[code {self.status_code}] \n{self.responce}"
