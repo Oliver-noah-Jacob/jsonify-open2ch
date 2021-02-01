aceaa45b73d62cd87eebb5a601fdb6904c00acad
Oliver-noah-Jacob <zeamaysmoney@yahoo.co.jp>
2021-02-01 13:33:38 +0900

    extend type

--- func/typedef.py
+++ func/typedef.py
@@ -1,16 +1,14 @@
+from enum import Enum
 import os
 import pathlib
 import time
-from typing import List, NamedTuple ,Union
+from typing import List, Literal, NamedTuple ,Union
 
 from cachecontrol import CacheControl 
 from cachecontrol.caches import FileCache
 import requests
 
-PathSegments = Union[
-    str, pathlib.Path, os.PathLike, 
-    List[str], List[pathlib.Path], List[os.PathLike]
-    ]
+# Type class
 
 class OpenAccessReturn(NamedTuple):
     url: str
@@ -19,3 +17,25 @@ class OpenAccessReturn(NamedTuple):
 
     def __str__(self) ->str:
         return f"url: {self.url}[code {self.status_code}] \n{self.responce}"
+
+class FileAccessMode(Enum):
+    rt = 'rt'
+    rb = 'rb'
+    at = 'at'
+    ab = 'ab'
+    wt = 'wt'
+    wb = 'wb'
+    xt = 'xt'
+    xb = 'xb'
+
+# Type alias
+
+PathSegments = Union[
+    str, pathlib.Path, os.PathLike, 
+    List[str], List[pathlib.Path], List[os.PathLike]
+    ]
+
+WritableMode = Literal[
+    FileAccessMode.at, FileAccessMode.ab, 
+    FileAccessMode.wt, FileAccessMode.wb,
+    ]
